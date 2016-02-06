c1cecd515a22da7081a0d1fcd4333b9b7fea6a80
James Harding <james@jamesharding.me>
2016-02-06 15:57:00 +0000

    Visual updates. Early stages of wallboard palette

--- routes/index.js
+++ routes/index.js
@@ -8,40 +8,40 @@ WallboardProvider = new WallboardProvider('localhost', 27017);
 
 router.get('/', function (req, res) {
 
-  WallboardProvider.findOne(function(error, wallboard) {
-    console.log(wallboard);
-    if( error ) console.log(error)
+    WallboardProvider.findOne(function (error, wallboard) {
+        console.log(wallboard);
+        if (error) console.log(error)
 
-    if (wallboard == null) {
-      console.log("restoring from json");
-      wb = require(__dirname + '/../public/wb.json');
-      res.render('index', { elems:  wb.elems});
-    } else {
-        console.log("restoring from db");
-        res.render('index', { elems:  wallboard.elems});
-    }
+        if (wallboard == null) {
+            console.log("restoring from json");
+            wb = require(__dirname + '/../public/wb.json');
+            res.render('index', {elems: wb.elems});
+        } else {
+            console.log("restoring from db");
+            res.render('index', {elems: wallboard.elems});
+        }
 
-  });
-
-});
-
-router.post('/', function(req,res) {
-  console.log(req.body);
-
-});
-
-router.post('/save', function (req,res) {
-  var file = __dirname + '/../public/wb.json';
-
-  jsonfile.writeFile(file, req.body.wb, function (err) {
-    console.error(err);
-  });
-
-  WallboardProvider.save(
-    req.body.wb,
-    function( error, docs) {
-        console.log(error);
     });
+
+});
+
+router.post('/', function (req, res) {
+    console.log(req.body);
+
+});
+
+router.post('/save', function (req, res) {
+    var file = __dirname + '/../public/wb.json';
+
+    jsonfile.writeFile(file, req.body.wb, function (err) {
+        console.error(err);
+    });
+
+    WallboardProvider.save(
+        req.body.wb,
+        function (error, docs) {
+            console.log(error);
+        });
 });
 
 module.exports = router;
