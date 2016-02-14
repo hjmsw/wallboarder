0ca405fa46c0ecfdcca16f65788a24f0e2adb5d2
James Harding <james@jamesharding.me>
2016-02-14 17:41:35 +0000

    Fix bug with colorpicker js

--- public/colorPicker.js
+++ public/colorPicker.js
@@ -7,17 +7,17 @@
 $(function () {
 
     //Init all color pickers
-    init = function(elem) {
+    init = function(e, elem) {
         $(".colorPicker").each(function () {
             $(this).tinycolorpicker();
 
             $(this).bind("change", function () {
                 if ($(this).hasClass("colorPickerBG")) {
                     $(this).parent().siblings(".box").css("background", $(this).data("plugin_tinycolorpicker").colorHex);
-                    if (elem) elem.css("background", $(this).data("plugin_tinycolorpicker").colorHex);
+                    if (typeof elem !== "undefined") elem.css("background", $(this).data("plugin_tinycolorpicker").colorHex);
                 } else if ($(this).hasClass("colorPickerTX")) {
                     $(this).parent().siblings(".box").css("color", $(this).data("plugin_tinycolorpicker").colorHex);
-                    if (elem) elem.css("color", $(this).data("plugin_tinycolorpicker").colorHex);
+                    if (typeof elem !== "undefined") elem.css("color", $(this).data("plugin_tinycolorpicker").colorHex);
                 }
             });
         });
@@ -31,7 +31,7 @@ $(function () {
         parent.append(generateMarkup("colorPickerBG", "Bg", $(elem).css("background-color")) +
             generateMarkup("colorPickerTX", "Aa", $(elem).css("color")));
 
-        init($(elem));
+        init(null, $(elem));
 
         function generateMarkup(c_class, c_text, c_color) {
             return  "<div class='colorPicker "+c_class+"'> \
