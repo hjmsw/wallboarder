60e874a4b09e28ac3334748d4a6a7d201b3f7690
James Harding <james@jamesharding.me>
2016-03-12 15:09:49 +0000

    Delete old wallboard provider code

--- providers/WallboardProvider.js
+++ providers/WallboardProvider.js
@@ -1,60 +1,3 @@
-//var Db = require('mongodb').Db;
-//var Connection = require('mongodb').Connection;
-//var Server = require('mongodb').Server;
-//var BSON = require('mongodb').BSON;
-//var ObjectID = require('mongodb').ObjectID;
-//
-//WallboardProvider = function(host, port) {
-//  this.db= new Db('node-mongo-wallboarder', new Server(host, port, {auto_reconnect: true}, {}));
-//  this.db.open(function(){});
-//};
-//
-//WallboardProvider.prototype.getCollection= function(callback) {
-//  this.db.collection('wallboards', function(error, wallboard_collection) {
-//    if( error ) callback(error);
-//    else callback(null, wallboard_collection);
-//  });
-//};
-//
-//WallboardProvider.prototype.save = function(wallboard, callback) {
-//    this.getCollection(function(error, wallboard_collection) {
-//      if( error ) callback(error)
-//      else {
-//        wallboard.created_at = new Date();
-//
-//        wallboard_collection.save(wallboard, function() {
-//           callback(null, wallboard);
-//        });
-//      }
-//    });
-//};
-//
-//WallboardProvider.prototype.findOne = function(slug, callback ) {
-//  this.getCollection(function(error, wallboard_collection) {
-//    if( error ) callback(error);
-//    else {
-//      wallboard_collection.findOne({url_slug: slug},function(error, result) {
-//        if(error) callback(error);
-//        else callback(null, result)
-//      });
-//    }
-//  });
-//};
-//
-//WallboardProvider.prototype.findById = function(id, callback) {
-//    this.getCollection(function(error, wallboard_collection) {
-//      if( error ) callback(error);
-//      else {
-//        wallboard_collection.findOne({_id: wallboard_collection.db.bson_serializer.ObjectID.createFromHexString(id)}, function(error, result) {
-//          if( error ) callback(error);
-//          else callback(null, result)
-//        });
-//      }
-//    });
-//};
-//
-//exports.WallboardProvider = WallboardProvider;
-
 var mongoose = require('mongoose');
 
 var util = require('util');
@@ -67,7 +10,6 @@ WallboardProvider = function() {
   db.on('error', console.error.bind(console, 'connection error:'));
   db.once('open', function() {
     console.log("connect");
-
 
   });
 
